0a76b279a02e4d2611bdbc08701341576e0044d9
SumaGuruvu <sumaguruvu@gmail.com>
2024-03-06 12:19:32 +0530

    basic array programs

---
+++ TMF/src/Array1.java
@@ -0,0 +1,22 @@
+//import java.util.Scanner;
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       //Scanner sc=new Scanner(System.in)
+		int a[][]= {{1,2,3},{4,5,6},{7,8}};
+		//System.out.println(a.length);
+		//System.out.println("-----------------");
+		for(int i=0;i<a.length;i++)
+		{
+			//System.out.println(a[i].length);
+			for(int j=0;j<a[j].length;j++)
+			{
+				//System.out.println(a[j].length);
+				//System.out.println("----------");
+				System.out.println(a[i][j]);
+			}
+		}
+	}
+
+}
