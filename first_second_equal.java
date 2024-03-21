5dbbd23550d4df3614dccf76b4dd8ff87be62517
SumaGuruvu <161418447+SumaGuruvu@users.noreply.github.com>
2024-03-21 16:25:40 +0530

    Add files via upload

---
+++ first_second_equal.java
@@ -0,0 +1,15 @@
+
+public class first_second_equal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a[]= {4,5,6,78,7};
+       int b[]= {7,8,9,23,4};
+       //System.out.println(a.length);
+    		   if(a[0]==b[b.length-1])
+    		   {
+    			   System.out.println("equal");
+    		   }
+    	 }
+
+}
